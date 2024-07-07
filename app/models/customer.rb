d32d21ecaadf9598e781225be77e0a78d89954ac
Mel <mel@Mels-MacBook-Pro.local>
2024-07-06 18:41:41 -0600

    Refine model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,6 @@ class Customer < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :address, presence: true
+
+  has_secure_password
 end
