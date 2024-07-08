a0c7f2cdaf53a3fde99a5e365b6c43f0f232a2f2
Mel <mel@Mels-MacBook-Pro.local>
2024-07-08 02:35:42 -0600

    Fix tea spec

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -6,5 +6,7 @@ RSpec.describe Tea, type: :model do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:temp) }
     it { should validate_presence_of(:brew_time) }
+    it { should have_many(:subscriptions).dependent(:destroy) }
+    it { should have_many(:customers).through(:subscriptions) }
   end
 end
