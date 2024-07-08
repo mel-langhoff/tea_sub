3578a7387b0ec7e14f9fafb009ab86dca67100b9
Mel <mel@Mels-MacBook-Pro.local>
2024-07-08 13:59:31 -0600

    #destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       get 'customers/:customer_id/subscriptions', to: 'subscriptions#index'
       post 'customers/:customer_id/subscriptions', to: 'subscriptions#create'
       patch 'customers/:customer_id/subscriptions/:id', to: 'subscriptions#update'
+      delete 'customers/:customer_id/subscriptions/:id', to: 'subscriptions#destroy'
     end
   end
 end
